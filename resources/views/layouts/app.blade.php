e9ad43c5a65d5c197d4cd823b93bad47761914bc
wisegear <lee@wisener.net>
2025-02-23 21:22:19 +0000

    Updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,7 @@
 
         <!-- Venobox css -->
         <link rel="stylesheet" href="{{ asset('assets/css/venobox.min.css') }}" type="text/css" media="screen" />
-        <link rel="preload" href="{{ asset('assets/js/venobox.min.js') }}" as="script">
+        <script type="text/javascript" src="{{ asset('assets/js/venobox.min.js') }}"></script>
 
     </head>
     <body class="flex flex-col font-sans antialiased min-h-screen max-w-screen-xl mx-auto px-4 xl:px-0 bg-white dark:bg-[#282c35]">
